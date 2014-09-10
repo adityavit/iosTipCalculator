3ca5250fa07fc93671d70171608da633fa2ffc5b
Aditya Bhatia <adib@yahoo-inc.com>
2014-09-10 08:42:51 -0700

    Initial Commit

---
+++ tipcalculator/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  tipcalculator
+//
+//  Created by Aditya Bhatia on 9/10/14.
+//  Copyright (c) 2014 Aditya Bhatia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Aditya Bhatia on 9/10/14.
+//  Copyright (c) 2014 Aditya Bhatia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
